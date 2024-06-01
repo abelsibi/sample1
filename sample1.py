72f94be3004797547868e6d75569b9322b9f851e
abelsibi <abelx.sibi@intel.com>
2024-06-01 13:22:24 +0530

    new

--- sample1.py
+++ sample1.py
@@ -24,10 +24,8 @@ df.to_csv("ipl auction details 2024.csv")
 
 im a king 
 
-<<<<<<< HEAD
 print("hi")
-=======
 #this is a new line 
->>>>>>> slave1
 line 28 - added from master
 line 29 added from slave
+hi bro
