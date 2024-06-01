8e79c63e3995eee5155b1897dc932d4b0eac73bc
abelsibi <abelx.sibi@intel.com>
2024-06-01 22:54:02 +0530

    new command added

--- sample1.py
+++ sample1.py
@@ -1,2 +1,14 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+url="https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
+response=requests.get(url)
+soup=BeautifulSoup(response.text,"lxml")
+product_name=soup.find_all("a",class_="title")
+product_name_list=[]
+for i in product_name:
+    product=i.text
+    product_name_list.append(product)
+print(product_name_list)
 
 
